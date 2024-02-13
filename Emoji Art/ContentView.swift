88dd0e1efd1e3d35463eb45abd3e4fe836d44bd3
Jarek <jaroslaw.wronka@luxmed.pl>
2024-02-13 14:23:11 +0100

    Initial Commit

---
+++ Emoji Art/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Emoji Art
+//
+//  Created by Boss on 13/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Emoji Art/Emoji_ArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji_ArtApp.swift
+//  Emoji Art
+//
+//  Created by Boss on 13/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Emoji_ArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
